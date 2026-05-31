3c3f8048f85902804298db6a9b9a80c5d6134897
Laurent Magnin <laurent1magnin@gmail.com>
2026-06-01 00:02:14 +0200

    #2 [PHP] fix: serve index.html from index.php to fix blank page

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php
-// Silence is golden.
+// Si le serveur web charge index.php par défaut, on sert le fichier principal index.html
+if (file_exists(__DIR__ . '/index.html')) {
+    readfile(__DIR__ . '/index.html');
+    exit;
+}
